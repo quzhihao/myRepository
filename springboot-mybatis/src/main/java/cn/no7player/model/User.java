2ce4395de2f8d31f3799cb2c5995d0dc7ec5dd3c
dahao <1427572667@qq.com>
2017-12-11 17:00:22 +0800

    b1第一次提交

---
+++ springboot-mybatis/src/main/java/cn/no7player/model/User.java
@@ -0,0 +1,36 @@
+package cn.no7player.model;
+
+/**
+ * Created by zl on 2015/8/27.
+ */
+public class User {
+    private String name;
+    private Integer id;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+   
+}
